7585b1d01a0dbc6b4eb06e8daf62360910b90bbf
Adrian Ågren <agrenadrian@gmail.com>
2021-11-21 21:57:36 +0100

    Begun prototyping
    I have added a nice movement system "PlayerMovement" using the newest Input System and I have played around with grappling
    
    [TODO]
    - Grapple to objects
    - Nicer jump

---
+++ Secret Santa Game Jam/Assets/Scripts/PlayerGrapple.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerGrapple : MonoBehaviour
+{
+    public Camera mainCamera;
+    public LineRenderer lineRenderer;
+    public DistanceJoint2D distanceJoint;
+
+    private Vector2 mousePoint;
+    private Vector2 projectedMousePoint;
+
+    private PlayerMovement movementScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceJoint.enabled = false;
+        movementScript = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (distanceJoint.enabled)
+        {
+            lineRenderer.SetPosition(1, transform.position);
+        }
+
+        projectedMousePoint = mainCamera.ScreenToWorldPoint(mousePoint);
+    }
+
+    public void Grapple(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Grapple Performed!");
+            lineRenderer.SetPosition(1, transform.position);
+            lineRenderer.SetPosition(0, projectedMousePoint);
+            distanceJoint.connectedAnchor = projectedMousePoint;
+            distanceJoint.enabled = true;
+            lineRenderer.enabled = true;
+
+            movementScript.enabled = false;
+        }
+        else if (context.canceled)
+        {
+            distanceJoint.enabled = false;
+            lineRenderer.enabled = false;
+
+            movementScript.enabled = true;
+        }
+    }
+
+    public void MousePoint(InputAction.CallbackContext context)
+    {
+        mousePoint = context.ReadValue<Vector2>();
+    }
+}
---
+++ Secret Santa Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private float speed = 6f;
+    private float jumpForce = 8f;
+    private bool facingRight = true;
+
+    private bool grounded;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y);
+
+        if (!facingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (facingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && grounded && rb.velocity.y == 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
